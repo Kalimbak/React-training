8962431d1d203dd894baaa2595593fcf3f6fd134
kevin kalimba <95082403+Kalimbak@users.noreply.github.com>
2022-08-24 12:46:50 +0200

    Add files via upload

---
+++ React-Add-meme-generator-app/src/ui/data/data.js
@@ -0,0 +1,40 @@
+export default {
+    "success": true,
+    "data": {
+        "memes": [
+            {
+                "id": "181913435",
+                "name": "umuhire ",
+                "url": "https://i.imgflip.com/30b1gx.jpg",
+                "width": 1200,
+                "height": 1200,
+                "box_count": 4
+            },
+            {
+                "id": "877432344",
+                "name": "new thing",
+                "url": "https://i.imgflip.com/1g8my4.jpg",
+                "width": 600,
+                "height": 908,
+                "box_count": 9
+            },
+            {
+                "id": "112126428",
+                "name": "all means ",
+                "url": "https://i.imgflip.com/1ur9b0.jpg",
+                "width": 1200,
+                "height": 800,
+                "box_count": 4
+            },
+            {
+                "id": "131087935",
+                "name": "picture",
+                "url": "https://i.imgflip.com/261o3j.jpg",
+                "width": 761,
+                "height": 1024,
+                "box_count": 9
+            },
+           
+        ]
+    }
+}
